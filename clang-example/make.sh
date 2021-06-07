023a5861befea939a86fe26167fde4f262bb327e
Peter Gielda <pgielda@antmicro.com>
2021-06-07 20:35:00 +0200

    Compile wasi-libc only once.

--- clang-example/make.sh
+++ clang-example/make.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/WebAssembly/wasi-libc.git
-cd wasi-libc ; make ; cd ..
+if [ ! -e wasi-libc ] ; then
+   git clone https://github.com/WebAssembly/wasi-libc
+   cd wasi-libc ; make ; cd ..
+fi
 clang --target=wasm32 -L. -nostdlib -Wl,--export-all -D__wasi__ -Wl,--allow-undefined -o test.wasm -I./wasi-libc/sysroot/include test.c wasi-libc/sysroot/lib/wasm32-wasi/libc.a wasi-libc/sysroot/lib/wasm32-wasi/crt1.o ./libclang_rt.builtins-wasm32.a
 
