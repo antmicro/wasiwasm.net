deef4e20d4f9aaedd64a9d7524fe680011d3e680
Peter Gielda <pgielda@antmicro.com>
2021-06-19 16:10:03 +0200

    Add mkbundle to make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,4 @@
 csc dotnet-webassembly/WebAssembly/*.cs dotnet-webassembly/WebAssembly/Runtime/*.cs dotnet-webassembly/WebAssembly/Runtime/Compilation/*.cs dotnet-webassembly/WebAssembly/Instructions/*.cs wasiwasm.cs -langversion:9.0 -unsafe -nowarn:8632
 
+mkbundle --simple -z --static --library /usr/lib64/libmono-native.so -L /usr/lib/mono/4.5 wasiwasm.exe -o wasiwasm
+
