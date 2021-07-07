186b56471739de89a22eeed7aafaf253b452063d
Peter Gielda <pgielda@antmicro.com>
2021-07-07 12:48:18 +0200

    Update make.sh
    Improve make.sh to support both /usr/lib and /usr/lib64

--- make.sh
+++ make.sh
@@ -1,4 +1,10 @@
 csc dotnet-webassembly/WebAssembly/*.cs dotnet-webassembly/WebAssembly/Runtime/*.cs dotnet-webassembly/WebAssembly/Runtime/Compilation/*.cs dotnet-webassembly/WebAssembly/Instructions/*.cs wasiwasm.cs -langversion:9.0 -unsafe -nowarn:8632
 
-mkbundle --simple -z --static --library /usr/lib64/libmono-native.so -L /usr/lib/mono/4.5 wasiwasm.exe -o wasiwasm
+if [ -e /usr/lib64/libmono-native.so ] ; then
+  NATIVE="/usr/lib64/libmono-native.so"
+else
+  NATIVE="/usr/lib/libmono-native.so"
+fi
+
+mkbundle --simple -z --static --library $NATIVE -L /usr/lib/mono/4.5 wasiwasm.exe -o wasiwasm
 
