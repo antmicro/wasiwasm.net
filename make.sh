1c6c47a4d3acebdd39be206bac4abd21fe8c9a6d
Peter Gielda <pgielda@antmicro.com>
2022-09-03 18:12:22 +0200

    bump dotnet-webassembly update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
+git submodule init
+git submodule update
+
 csc dotnet-webassembly/WebAssembly/*.cs dotnet-webassembly/WebAssembly/Runtime/*.cs dotnet-webassembly/WebAssembly/Runtime/Compilation/*.cs dotnet-webassembly/WebAssembly/Instructions/*.cs wasiwasm.cs -langversion:9.0 -unsafe -nowarn:8632
 
 if [ -e /usr/lib64/libmono-native.so ] ; then
